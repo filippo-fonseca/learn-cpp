fd992d77bbb54d866f8e41d7886b4e2b8edd658e
Filippo Fonseca <filifonsecacagnazzo@gmail.com>
2024-08-01 11:52:56 -0600

    while loop stuff

---
+++ course/while.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    std::string name;
+
+    while (name.empty()) {
+        std::cout << "Please enter your name: ";
+        std::getline(std::cin, name);
+    }
+
+    std::cout << "Hello " << name << "!" << std::endl;
+
+
+    return 0;
+}
