c7925c403ec6965158a16726eaf80ae2873b8ac0
Filippo Fonseca <filifonsecacagnazzo@gmail.com>
2024-08-01 11:48:39 -0600

    new file struct

---
+++ course/strings.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+
+int main() {
+    std::string name;
+
+    std::cout << "Enter your name: ";
+    std::getline(std::cin, name);
+
+    if (name.length() > 12) {
+        std::cout << "Your name is too long." << std::endl;
+    } else {
+        std::cout << "Hello " << name << "!" << std::endl;
+    }
+
+
+   return 0;
+}
