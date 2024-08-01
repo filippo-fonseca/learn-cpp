974d9473c1c1c2e4652ef278c1f4520f9e186cc0
Filippo Fonseca <filifonsecacagnazzo@gmail.com>
2024-08-01 13:39:05 -0600

    add mem address prac

---
+++ course/memAddress.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    std::string name = "Bro";
+    int age = 18;
+    bool student = true;
+
+    std::cout << &name << std::endl;
+    std::cout << &age << std::endl;
+    std::cout << &student << std::endl;
+
+    return 0;
+}
